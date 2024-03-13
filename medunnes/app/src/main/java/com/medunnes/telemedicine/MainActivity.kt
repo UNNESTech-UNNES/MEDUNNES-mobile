9eaf5916b9c379bcc05fe0f1a6c808c8b6785e93
ReyhanHerdi <reyhanherdiyanto180603@students.unnes.ac.id>
2024-03-13 13:18:24 +0700

    initiate project

---
+++ medunnes/app/src/main/java/com/medunnes/telemedicine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.medunnes.telemedicine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
